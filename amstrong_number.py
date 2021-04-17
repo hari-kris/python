de1ef40a8bc97f07bac1da88dd299adc83ba77eb
Harinath Krishnamoorthy <harinath@buddi.ai>
2021-04-17 22:41:35 +0530

    Pushing Armstrong number, sum of digits

--- amstrong_number.py
+++ amstrong_number.py
@@ -9,3 +9,20 @@ abcd... = an
 Example: 153 = 1*1*1 + 5*5*5 + 3*3*3
 153 is an Armstrong number of order 3.
 """
+
+
+def armstrong_number_checker(number):
+    computed_number = 0
+    original_number = number
+    while number > 0:
+        remainder = number % 10
+        computed_number = computed_number + (remainder*remainder*remainder)
+        number = number // 10
+    if computed_number == original_number:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(armstrong_number_checker(370))
+    print(armstrong_number_checker(121))
--- sum_of_digits.py
+++ sum_of_digits.py
@@ -2,3 +2,16 @@
 Given a number, find the sum of its digits. Take the number as
 an input from the user
 """
+
+
+def sum_of_digits(number):
+    total = 0
+    while number > 0:
+        digit = number % 10
+        number = number // 10
+        total = total + digit
+    return total
+
+
+if __name__ == '__main__':
+    print(sum_of_digits(125))
