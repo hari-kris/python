f186b968998d7c8c83a6dbbd02ad147314bdae45
Harinath Krishnamoorthy <harinath@buddi.ai>
2021-04-18 00:58:28 +0530

    Pushing Detecting Duplicate Element and counting the frequency for the same

--- detect_duplicate_element.py
+++ detect_duplicate_element.py
@@ -2,3 +2,18 @@
  Given an array which may contain duplicates, print all elements
 and their frequencies
 """
+
+
+def detect_duplicate_frequency(input_array):
+    freq_array = {}
+    for each in input_array:
+        if freq_array.get(each) is None:
+            freq_array[each] = 1
+        else:
+            freq_array[each] = freq_array[each] + 1
+    return freq_array
+
+
+if __name__ == '__main__':
+    my_list = [1, 1, 2, 2, 3, 4, 5, 5]
+    print(detect_duplicate_frequency(my_list))
