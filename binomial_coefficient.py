740f8c661cd7173b656cc3e6dcae37c2f6fa3df6
Harinath Krishnamoorthy <harinath@buddi.ai>
2021-04-18 09:48:24 +0530

    Pushing Binomail Coefficient

--- binomial_coefficient.py
+++ binomial_coefficient.py
@@ -2,3 +2,20 @@
  Given two numbers n and r, find the value of nCr (binomial
 coefficient: nCr = (n!) / (r! * (n-r)!))
 """
+
+
+def compute_factorial(number):
+    fact = 1
+    if number == 1:
+        return fact
+    else:
+        return number * compute_factorial(number - 1)
+
+
+def compute_binomial_coefficient(n, r):
+    return compute_factorial(n) / (compute_factorial(r) * compute_factorial(n-r))
+
+    
+if __name__ == '__main__':
+    # print(compute_factorial(120))
+    print(compute_binomial_coefficient(7,2))
